799204631df8b6e5bba670eac4863c12719faf26
Slim Babay <slimbabay@hotmail.com>
2015-03-15 22:53:38 +0000

    Updated DatabaseSetup.sql, added comments and removed redundant super user
    git-tfs-id: [https://slimbabay.visualstudio.com]$/Project 3 - ENGI3675;C64

--- Database/databasecreation.sql
+++ Database/databasecreation.sql
@@ -1,13 +1,12 @@
+--Initialize starting conditions by deleting database
 drop database if exists "Assignment3";
 
+--Initilize User
 drop role if exists "Assignment3";
 create role "Assignment3" login;
 comment on role "Assignment3" is 'Resctricted ISS app pool user';
 
-drop role if exists "George";
-create role "George" login superuser;
-comment on role "George" is 'Personal developer superuser';
-
+--Creating Database and granting permissions
 create database "Assignment3";
 comment on database "Assignment3" is 'Database for Assigment3';
 
--- Proj3DBAccess/Assignment3.cs
+++ Proj3DBAccess/Assignment3.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Proj3DBAccess
 {
-    public class Class1
+    public class Assignment3
     {
     }
 }
