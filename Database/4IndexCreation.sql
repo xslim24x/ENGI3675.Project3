7c5667fec9f70bcf7ea48b0cc7bdfdbd429e8300
George <georgedziouba@hotmail.ca>
2015-03-19 20:56:29 +0000

    index updates
    git-tfs-id: [https://slimbabay.visualstudio.com]$/Project 3 - ENGI3675;C79

--- Database/4IndexCreation.sql
+++ Database/4IndexCreation.sql
@@ -1,10 +1,9 @@
-﻿
---creating indexes
+﻿--creating indexes
 drop index if exists hpds_indexed1;
 CREATE INDEX hpds_indexed1 ON hpds_indexed(continent);
 
 drop index if exists hpds_indexed2;
 CREATE INDEX hpds_indexed2 ON hpds_indexed(amount);
 
-drop index hpds_indexed2;
+drop index if exists hpds_indexed3;
 CREATE INDEX hpds_indexed3 ON hpds_indexed(sector);
